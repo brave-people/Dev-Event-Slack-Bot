a897af4105c244717d8ba85adb67490616bd7686
Nuung <qlgks1@naver.com>
2022-06-19 14:06:34 +0900

    [update] slack API 활용해 모든 ch 가져와 분기대로 모두 메시지 보내기 테스트

--- src/core/core.py
+++ src/core/core.py
@@ -22,34 +22,19 @@ class SlackAPI:
         ssl_context = ssl.create_default_context(cafile=certifi.where())
         self.client = WebClient(config['slack_token'], ssl=ssl_context)  # 슬랙 클라이언트 인스턴스 생성
         self.__dict__.update(kwargs)  # 변동 가능성 있는 변수값 위해 kwargs 세팅
-        
-    def get_channel_id(self, channel_name):
+
+
+    def get_channel_info(self) -> list:
         """
-        슬랙 채널ID 조회
+        슬랙에 해당 봇을 추가한 워크스페이스의 모든 채널 info 가져오기 
         """
         # conversations_list() 메서드 호출
-        result = self.client.conversations_list()
+        result = self.client.conversations_list() # slack bot에 대한 정보 나열
+        
         # 채널 정보 딕셔너리 리스트
-        channels = result.data['channels']
-        # 채널 명이 'test'인 채널 딕셔너리 쿼리
-        channel = list(filter(lambda c: c["name"] == channel_name, channels))[0]
-        # 채널ID 파싱
-        channel_id = channel["id"]
-        return channel_id
+        channels = result.data['channels'] # 그 중 slack bot이 추가된 channel에 대한 리스트업
+        return channels
 
-    def get_message_ts(self, channel_id, query):
-        """
-        슬랙 채널 내 메세지 조회
-        """
-        # conversations_history() 메서드 호출
-        result = self.client.conversations_history(channel=channel_id)
-        # 채널 내 메세지 정보 딕셔너리 리스트
-        messages = result.data['messages']
-        # 채널 내 메세지가 query와 일치하는 메세지 딕셔너리 쿼리
-        message = list(filter(lambda m: m["text"]==query, messages))[0]
-        # 해당 메세지ts 파싱
-        message_ts = message["ts"]
-        return message_ts
 
     def post_text_message(self, channel_id: str, msg: str) -> SlackResponse:
         """
@@ -64,6 +49,41 @@ class SlackAPI:
         except SlackApiError as e:
             # You will get a SlackApiError if "ok" is False
             assert e.response["error"]    # str like 'invalid_auth', 'channel_not_found'
+
+
+    # ================================================================= #
+    # 이하 Optional 한 method 들 
+    # ================================================================= #
+
+
+    def get_channel_id_by_name(self, channel_name):
+        """
+        슬랙 채널명으로 채널ID 조회
+        """
+        # conversations_list() 메서드 호출
+        result = self.client.conversations_list()
+        # 채널 정보 딕셔너리 리스트
+        channels = result.data['channels']
+        # 채널 명이 'test'인 채널 딕셔너리 쿼리
+        channel = list(filter(lambda c: c["name"] == channel_name, channels))[0]
+        # 채널ID 파싱
+        channel_id = channel["id"]
+        return channel_id
+
+
+    def get_message_ts(self, channel_id, query):
+        """
+        슬랙 채널 내 메세지 조회
+        """
+        # conversations_history() 메서드 호출
+        result = self.client.conversations_history(channel=channel_id)
+        # 채널 내 메세지 정보 딕셔너리 리스트
+        messages = result.data['messages']
+        # 채널 내 메세지가 query와 일치하는 메세지 딕셔너리 쿼리
+        message = list(filter(lambda m: m["text"]==query, messages))[0]
+        # 해당 메세지ts 파싱
+        message_ts = message["ts"]
+        return message_ts
 
 
     def post_thread_message(self, channel_id, message_ts, text):
--- src/main.py
+++ src/main.py
@@ -3,4 +3,8 @@
 from core.core import SlackAPI
 
 slack = SlackAPI(conf="test")
-slack.post_text_message("C034R2SQRDY", "테스트")
+target_channels = slack.get_channel_info()
+for target_ch in target_channels:
+    # 채널에 추가하고, 멤버로 초대를 해야만 message를 보낼 수 있다
+    if target_ch['is_member']:
+        slack.post_text_message(target_ch['id'], f"테스트 to {target_ch['name']}")
