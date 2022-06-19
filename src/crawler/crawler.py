8efeb24e0a0c714e2eec9bbce1c4232ad05b5e28
Nuung <qlgks1@naver.com>
2022-06-19 17:32:42 +0900

    [update] 크롤러 기본 펑셔널 함수 정의 및 업데이트

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -1,9 +1,9 @@
 
 # python lib
-import os
+import os, csv
 from requests import get
 from bs4 import BeautifulSoup
-from typing import List
+from typing import Any, List
 
 # custom lib
 from core.exception_handler import exception_handler
@@ -13,37 +13,53 @@ class Crawler:
     범용성 X, 특수 목적, 특수 url 크롤링 및 타겟 변화감지 모니터링
     """
     STANDARD_GIT_URL = "https://github.com"
-
+    FILE_PATH = f'{os.environ.get("base_dir")}/data/csv/standard_url.csv'
 
     def __init__(self) -> None:
         pass
 
 
-
-    def get_html(self):
+    def get_html(self, target_url=None):
         """
         특정 url을 통해 특정 data 들고오기, header 값 필요 X
         """
         try:
-            url = os.environ.get("event_check_url")
+            url = os.environ.get("event_check_url") if isinstance(target_url, type(None)) else target_url
             res = get(url, timeout=3)
             return BeautifulSoup(res.content, 'lxml')
         except Exception as exc:
             return exception_handler(exc)
 
 
-    def get_standard_data(self):
+    def get_standard_data(self) -> list[str]:
         """
         변화의 기준이 되는 standard 데이터 가져오기, 없으면 만들기
         """
-        pass
+
+        # 존재하면
+        if os.path.isfile(self.FILE_PATH):
+            with open(self.FILE_PATH, 'r', encoding='utf-8') as file:
+                return file.readlines()
+        # 존재하지 않으면
+        else:  
+            return self.set_standard_data(self.data_parsing())
+
+
+    def set_standard_data(self, issue_url_list: list[str]) -> list[str]:
+        """
+        변화의 기준이 되는 standard 데이터 쓰기
+        """
+        with open(self.FILE_PATH, 'w', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(issue_url_list)
+
+        return issue_url_list
 
 
     def data_parsing(self) -> List[str]:
         """
         DOM 중 타겟 DOM의 데이터만 가져오기, 여기서는 issue의 url만 가져올 것
         """
-        
         html = self.get_html()
         main_div = html.find("div", {"id": "repo-content-pjax-container"})
         issue_lists = main_div.find("div", {"class": "js-navigation-container js-active-navigation-container"}).find_all('div')
@@ -56,9 +72,35 @@ class Crawler:
         return issue_url_list
 
 
+    def data_detail_parsing(self, target_url):
+        """
+        변화감지로 잡힌 issue의 url에서 detail한 정보를 parsing해 옴, hook 보낼 정보들임
+        """
+        html = self.get_html(target_url=target_url)
+
+
     def data_compare(self):
         """
         standard 데이터와 지금 긁어온 데이터 리스트 비교, 달라지면 변화 감지
         그리고 standard를 update 한다. 
         return - 감지된 변화에 대해 훅 보낼 데이터 
         """
+
+        # step1. standard data 가져오기
+        standard_data_list = self.get_standard_data()
+
+        # step2. target url에서 issue의 url list 가져오기
+        new_issue_url_list = self.data_parsing()
+
+        # step3. 두 list에서 다른 값 가져오기 (다른 url 가져오기)
+
+
+        # step4. 다른 url을 타고 들어가서 상세 정보를 한 번 더 파싱해오기
+
+        # 해당 상세 정보를 return 하기
+
+
+
+def crawler_run():
+    crawl = Crawler()
+    return crawl.data_compare()
--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@
 import os
 from core.slack import SlackAPI
 from core.config import set_env
+from crawler.crawler import crawler_run
 
 # 환경변수 세팅
 set_env(["slack_token", "event_check_url"])
 
 # monitor
-
-
+msg_txt = crawler_run()
 
 # slack 
 slack = SlackAPI(debug=True)
@@ -17,7 +17,7 @@ target_channels = slack.get_channel_info()
 for target_ch in target_channels:
     # 채널에 추가하고, 멤버로 초대를 해야만 message를 보낼 수 있다
     if target_ch['is_member']: 
-        slack.post_text_message(target_ch['id'], f"테스트 to {target_ch['name']}")
+        slack.post_text_message(target_ch['id'], f"{msg_txt} to {target_ch['name']}")
 
 
 
