3a29ba2783883e6d49d7022947fa788f32476b28
Nuung <qlgks1@naver.com>
2022-11-10 16:39:49 +0900

    oauth url hot fix v5

--- oauth/routes/index.js
+++ oauth/routes/index.js
@@ -23,7 +23,7 @@ mongoose
  */
 router.get('/auth/slack', async (_, res) => {
     const scopes = 'calls:write,channels:read,chat:write,groups:read,im:read,mpim:read&amp;user_scope=&amp';
-    const redirectUrl = `https://${domainName}/auth/slack/callback`;
+    const redirectUrl = `https://${domainName}/auth/slack/callback&amp`;
     const clientId = process.env.SLACK_CLIENT_ID;
     const oauthUrl = `https://slack.com/oauth/v2/authorize?scope=${scopes};redirect_uri=${redirectUrl};client_id=${clientId}`;
     return res.render('index', { oauthUrl });
