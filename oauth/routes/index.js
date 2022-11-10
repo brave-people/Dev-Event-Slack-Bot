07eb79d037d8bf6b76c2f0140c4be58f563b9964
Nuung <qlgks1@naver.com>
2022-11-10 16:04:20 +0900

    docker compose nginx log path, route value change

--- oauth/routes/index.js
+++ oauth/routes/index.js
@@ -9,13 +9,13 @@ const slackClientId = process.env.SLACK_CLIENT_ID
 
 // db connect
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "userInfo" // 이 이름으로 db가 생성됩니다.
-  })
-  .then(() => console.log(`mongoDB connected`))
-  .catch((err) => console.error(err));
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "userInfo" // 이 이름으로 db가 생성됩니다.
+    })
+    .then(() => console.log(`mongoDB connected`))
+    .catch((err) => console.error(err));
 
 
 /**
@@ -24,11 +24,11 @@ mongoose
 router.get('/auth/slack', async (_, res) => {
     const scopes = 'identity.basic,identity.email';
     const redirectUrl = `https://${domainName}/auth/slack/callback`;
-    
-    //Here you build the url. You could also copy and paste it from the Manage Distribution page of your app.
-    const oauthUrl = `https://slack.com/oauth/v2/authorize?client_id=${slackClientId}&user_scope=${scopes}&redirect_uri=${redirectUrl}`;
 
-    res.render('index', {oauthUrl});
+    //Here you build the url. You could also copy and paste it from the Manage Distribution page of your app.
+    // https://slack.com/oauth/v2/authorize?client_id=2844308222017.3909658800162&scope=calls:write,chat:write,channels:read,groups:read,mpim:read,im:read&user_scope=chat:write,im:read,identity.basic,calls:write,channels:read
+    const oauthUrl = `https://slack.com/oauth/v2/authorize?client_id=${slackClientId}&user_scope=${scopes}&redirect_uri=${redirectUrl}`;
+    return res.render('index', { oauthUrl });
 });
 
 
@@ -48,10 +48,10 @@ router.get('/auth/slack/callback', async (req, res) => {
         });
 
         // At this point you can assume the user has logged in successfully with their account.
-        res.status(200).send(`<html><body><p>You have successfully logged in with your slack account! Here are the details:</p><p>Response: ${JSON.stringify(response)}</p><p>Identity: ${JSON.stringify(identity)}</p></body></html>`);
+        return res.status(200).send(`<html><body><p>You have successfully logged in with your slack account! Here are the details:</p><p>Response: ${JSON.stringify(response)}</p><p>Identity: ${JSON.stringify(identity)}</p></body></html>`);
     } catch (eek) {
         console.log(eek);
-        res.status(500).send(`<html><body><p>Something went wrong!</p><p>${JSON.stringify(eek)}</p>`);
+        return res.status(500).send(`<html><body><p>Something went wrong!</p><p>${JSON.stringify(eek)}</p>`);
     }
 });
 
